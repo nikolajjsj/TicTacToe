c527943fd0810f32c7174a0992f9af61b03c0096
nikolajjsj <nikolaj.app.dev@gmail.com>
2020-09-16 09:50:34 +0200

    Updates

---
+++ precache-manifest.54b4cdab4c3804356f2da2a3b10749e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d286255e86a9da03ae52305615c121",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "118500ddf0882e965378",
+    "url": "/TicTacToe/static/css/main.9ec16c32.chunk.css"
+  },
+  {
+    "revision": "bda7d3999047cfc7c72e",
+    "url": "/TicTacToe/static/js/2.3d54d053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.3d54d053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118500ddf0882e965378",
+    "url": "/TicTacToe/static/js/main.dd2a302c.chunk.js"
+  },
+  {
+    "revision": "35d19f0c13e4574912e8",
+    "url": "/TicTacToe/static/js/runtime-main.29f4516c.js"
+  }
+]);
